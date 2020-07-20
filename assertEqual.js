a42908613aa19909811e910ee56445e18bb3b96b
Juan Nava <juan.ns17@gmail.com>
2020-07-20 21:55:11 +0000

    added use of literals to strings

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,11 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-
+  let checkmark = String.fromCodePoint(0x2705)
+  let errormark = String.fromCodePoint(0x274C)
   if (actual === expected){
-    console.log(String.fromCodePoint(0x2705) + "Assertion Passed: " + actual + " === " + expected);
+    console.log(`${checkmark} Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(String.fromCodePoint(0x274C) + "Assertion Failed: " + actual + " !== " + expected);
+    console.log(`${errormark} Assertion Failed: ${actual} !== ${expected}`);;
   }
 };
 
