5ff71790c254ab504366de71a965ee0aa26c5040
Juan Nava <juan.ns17@gmail.com>
2020-07-23 20:11:30 +0000

    Iterate through each element in source array modifying it according to function and passing onto new array

---
+++ map.js
@@ -0,0 +1,35 @@
+const assertArraysEqual = function(actual, expected) {
+  if (actual && expected) {
+    let checkmark = String.fromCodePoint(0x2705);
+    let errormark = String.fromCodePoint(0x274C);
+    
+    for (let i = 0; i < actual.length; i++) {
+      if (actual[i] !== expected[i]) {
+        console.log(`${errormark} Assertion Failed: ${actual} !== ${expected}`);
+        return;
+      }
+    }
+    console.log(`${checkmark} Assertion Passed: ${actual} === ${expected}`);
+    return;
+  } else {
+    console.log('Array undefined. Makre sure actual and expected arrays are entered');
+  }
+};
+
+
+const map = (arr, callBack) => {
+  const results = [];
+  
+  for (let i of arr) {
+    results.push(callBack(i));
+  }
+
+  return results;
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
+assertArraysEqual(results1,["g", "c", "t", "m", "t"]);
