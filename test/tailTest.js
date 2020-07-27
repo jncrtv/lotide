addde4ad3f5f659a0d5384b95796ef2ca30ec444
Juan Nava <juan.ns17@gmail.com>
2020-07-27 20:48:37 +0000

    removed old code

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,15 +1,3 @@
-// const tail = require('../tail');
-// const assertArraysEqual = require('../assertArraysEqual');
-
-
-// assertArraysEqual(tail([5]), '', true);
-// assertArraysEqual(tail([5,3]), [3], true);
-// assertArraysEqual(tail([5,3,4,4]), [3], false);
-// assertArraysEqual(tail([5,3,4,4]), [3,4,4], true);
-// assertArraysEqual(tail([]), [3], false);
-// assertArraysEqual(tail([4,4]), [3], false);
-
-
 const assert = require('chai').assert;
 const tail = require('../tail');
 
