9d32e1be0747f2b60e41facf95175d677ed78a44
Juan Nava <juan.ns17@gmail.com>
2020-07-27 20:22:19 +0000

    implemented mocha and chai test structure

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,12 +1,41 @@
+// const tail = require('../tail');
+// const assertArraysEqual = require('../assertArraysEqual');
+
+
+// assertArraysEqual(tail([5]), '', true);
+// assertArraysEqual(tail([5,3]), [3], true);
+// assertArraysEqual(tail([5,3,4,4]), [3], false);
+// assertArraysEqual(tail([5,3,4,4]), [3,4,4], true);
+// assertArraysEqual(tail([]), [3], false);
+// assertArraysEqual(tail([4,4]), [3], false);
+
+
+const assert = require('chai').assert;
 const tail = require('../tail');
-const assertArraysEqual = require('../assertArraysEqual');
+
+describe("#tail", () => {
+  it("return [] for [5]", () => {
+    assert.deepEqual(tail([5]), []);
+  })
+
+  it("return [3] for [5, 3]", () => {
+    assert.deepEqual(tail([5,3]), [3]);
+  })
+
+  it("return [3, 4, 4] for [5, 3, 4, 4]", () => {
+    assert.deepEqual(tail([5,3,4,4]), [3,4,4]);
+  })
+
+  it("return [] for []", () => {
+    assert.deepEqual(tail([]), []);
+  })
+
+  it("return [4] for [4, 4]", () => {
+    assert.deepEqual(tail([4,4]), [4]);
+  })
 
 
-assertArraysEqual(tail([5]), '', true);
-assertArraysEqual(tail([5,3]), [3], true);
-assertArraysEqual(tail([5,3,4,4]), [3], false);
-assertArraysEqual(tail([5,3,4,4]), [3,4,4], true);
-assertArraysEqual(tail([]), [3], false);
-assertArraysEqual(tail([4,4]), [3], false);
+
+});
 
 
